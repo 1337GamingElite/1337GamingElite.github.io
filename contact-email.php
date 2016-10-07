d19eda9c51d2e261e29834442c552268b36c012e
1337GamingElite <1337gamingelite@gmail.com>
2016-10-06 21:29:47 -0600

    Add php code

--- contact-email.php
+++ contact-email.php
@@ -1,5 +1,13 @@
 <?php
 
+    $first_name = $_POST['first_name'];
+    $last_name = $_POST['last_name'];
+    $email = $_POST['email'];
+    $message = $_POST['message'];
+    $to = "1337gamingelite@gmail.com";
+    $subject = $_POST['subject'];
 
+    mail($to, $subject, $message, "From: " . $first_name . $last_name);
+    echo "Your Message has been Sent";
 
-?> 
+?>
