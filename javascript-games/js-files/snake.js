b41f0a8e919b7884907a383e9924b1eab90e17eb
1337GamingElite <1337GamingElite@gmail.com>
2016-11-05 22:10:58 -0600

    Finished the Snake Game

--- javascript-games/js-files/snake.js
+++ javascript-games/js-files/snake.js
@@ -1,87 +1,223 @@
-//8:13
-// Size of the Game Area
+// Grid Size
 var COLS = 26, ROWS = 26;
 
-//Object IDs
-var EMPTY = 0, SNAKE = 1, FRUIT = 1;
+// Game ID's
+var EMPTY = 0, SNAKE = 1, FRUIT = 2;
 
-// The grid that would be the game area
+// Direction
+var LEFT  = 0, UP = 1, RIGHT = 2, DOWN = 3;
+
+// Keys
+var KEY_LEFT = 65, KEY_UP = 87, KEY_RIGHT = 68, KEY_DOWN = 83;
+
+// Game Variables
+var canvas, ctx, keystate, frames, score;
+
 var grid = {
+	width: null,  // Number of Columns
+	height: null, //Number of Rows
+	_grid: null,
 
-    width: null,
-    height: null,
-    _grid: null,
+	init: function(d, c, r) {
+		this.width = c;
+		this.height = r;
+		this._grid = [];
+		for (var x=0; x < c; x++) {
+			this._grid.push([]);
+			for (var y=0; y < r; y++) {
+				this._grid[x].push(d);
+			}
+		}
+	},
 
-    init: function(d, c, r) {
-        this.width = c;
-        this.height = r;
+	set: function(val, x, y) {
+		this._grid[x][y] = val;
+	},
 
-        this._grid = [];
+	get: function(x, y) {
+		return this._grid[x][y];
+	}
+}
 
-        // Creates the grid
-        for (var x = 0; x < c; x++) {
-            this._grid.push([]);
-            for (var y = 0; y < r; y++){
-                this._grid[x].push(d);
-            }
-        }
-    },
+var snake = {
+	direction: null, // The direction of the snake
+	last: null,		 // Last element in the queue (Snake is First in first out)
+	_queue: null,
 
-    set: function(val, x, y) {
-        this._grid[x][y] = val;
-    },
+	init: function(d, x, y) {
+		this.direction = d;
+		this._queue = [];
+		this.insert(x, y);
+	},
 
-    get: function(x, y) {
-        return this._grid[x][y];
-    }
+	insert: function(x, y) {
+		// unshift prepends an element to an array
+		this._queue.unshift({x:x, y:y});
+		this.last = this._queue[0];
+	},
 
+	remove: function() {
+		// pop returns the last element of an array
+		return this._queue.pop();
+	}
 };
 
-// The Snake
-var snake = {
-
-    direction: null,
-    _queue: null,
-
-    init: function(d, x, y) {
-
-    },
-
-    insert: function(x, y) {
-
-    },
-
-    remove: function() {
-
-    }
-
+// Sets food at a random empty space in the game
+function setFood() {
+	var empty = [];
+	// Finds empty cells
+	for (var x=0; x < grid.width; x++) {
+		for (var y=0; y < grid.height; y++) {
+			if (grid.get(x, y) === EMPTY) {
+				empty.push({x:x, y:y});
+			}
+		}
+	}
+	// Chooses a random cell
+	var randpos = empty[Math.round(Math.random()*(empty.length - 1))];
+	grid.set(FRUIT, randpos.x, randpos.y);
 }
 
-// Makes food on the grid
-function generateFood() {
-
+// Starts the game
+function main(id) {
+	// Makes Canvas
+	canvas = document.getElementById("gameCanvas");
+	canvas.width = COLS*20;
+	canvas.height = ROWS*20;
+	ctx = canvas.getContext("2d");
+    var div = document.getElementById(id);
+	// Adds canvas to body element
+	div.appendChild(canvas);
+	// sets an base font for bigger score display
+	ctx.font = "20px Comic Sans MS";
+	frames = 0;
+	keystate = {};
+	// Keyboard input
+	document.addEventListener("keydown", function(evt) {
+		keystate[evt.keyCode] = true;
+	});
+	document.addEventListener("keyup", function(evt) {
+		delete keystate[evt.keyCode];
+	});
+	// Starts Game Loop + Initiates Objects
+	init();
+	loop();
 }
 
-// GAME FUNCTIONS
-function main() {
-    // The game's main function, it starts here
-    //window.alert("Testing 1...2...3... SKT WIN WORLDS!!!")
-}
-
+// Resets or Sets game objects
 function init() {
-    // Runs when the game starts
+	score = 0;
+	grid.init(EMPTY, COLS, ROWS);
+	var sp = {x:Math.floor(COLS/2), y:ROWS-1};
+	snake.init(UP, sp.x, sp.y);
+	grid.set(SNAKE, sp.x, sp.y);
+	setFood();
 }
 
+function gameOver() {
+    var gameOverMessage = confirm("Congrats, You have gotten " + score + " points. Do you want to continue?");
+    if (gameOverMessage) {
+        init();
+    } else {
+        window.location = "../../index.html";
+    }
+}
+
+// Loops throuch update and draw functions
 function loop() {
-    // Loops the update & draw functions
+	update();
+	draw();
+	// When ready to redraw the canvas call the loop function
+	// Runs about 60 frames a second
+	window.requestAnimationFrame(loop, canvas);
 }
 
+// Updates game logic
 function update() {
-    // Updates game logic (score, positions, etc.)
+	frames++;
+	// Changes the snake's direction
+	if (keystate[KEY_LEFT] && snake.direction !== RIGHT) {
+		snake.direction = LEFT;
+	}
+	if (keystate[KEY_UP] && snake.direction !== DOWN) {
+		snake.direction = UP;
+	}
+	if (keystate[KEY_RIGHT] && snake.direction !== LEFT) {
+		snake.direction = RIGHT;
+	}
+	if (keystate[KEY_DOWN] && snake.direction !== UP) {
+		snake.direction = DOWN;
+	}
+	// Every 10 frames would update game state
+	if (frames % 5 === 0) {
+		// Pop the last element from the snake queue
+		var nx = snake.last.x;
+		var ny = snake.last.y;
+		// Updates the position of the snake
+		switch (snake.direction) {
+			case LEFT:
+				nx--;
+				break;
+			case UP:
+				ny--;
+				break;
+			case RIGHT:
+				nx++;
+				break;
+			case DOWN:
+				ny++;
+				break;
+		}
+		// Checks all gameover conditions
+		if (0 > nx || nx > grid.width-1  ||
+			0 > ny || ny > grid.height-1 ||
+			grid.get(nx, ny) === SNAKE
+		) {
+			return gameOver();
+		}
+		// Check whether the new position are on the fruit item
+		if (grid.get(nx, ny) === FRUIT) {
+			// Increment the score and sets a new fruit position
+			score++;
+			setFood();
+		} else {
+			// Take out the first item from the snake queue
+			var tail = snake.remove();
+			grid.set(EMPTY, tail.x, tail.y);
+		}
+		// Add a snake id at the new position and append it to the snake queue
+		grid.set(SNAKE, nx, ny);
+		snake.insert(nx, ny);
+	}
 }
 
+// Renders the grid
 function draw() {
-    // Displays the game/graphics on the screen
+	// Calculate tile-width and -height
+	var tw = canvas.width/grid.width;
+	var th = canvas.height/grid.height;
+	// Draws all the cells
+	for (var x=0; x < grid.width; x++) {
+		for (var y=0; y < grid.height; y++) {
+			// Sets the cell colour
+			switch (grid.get(x, y)) {
+				case EMPTY:
+					ctx.fillStyle = "#fff";
+					break;
+				case SNAKE:
+					ctx.fillStyle = "#00f";
+					break;
+				case FRUIT:
+					ctx.fillStyle = "#f00";
+					break;
+			}
+			ctx.fillRect(x*tw, y*th, tw, th);
+		}
+	}
+	// changes the fillstyle once more and draws the score
+	// message to the canvas
+	ctx.fillStyle = "#000";
+	ctx.fillText("SCORE: " + score, 10, canvas.height - 10);
 }
 
-main();
+main("game");
